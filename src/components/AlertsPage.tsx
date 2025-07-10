21f9dffc8fce69002d98a51e76bd6e7fb01a4b0e
002alferro002 <002.alferro.002@gmail.com>
2025-07-10 12:35:54 +0300

    Updated AlertsPage.tsx

--- src/components/AlertsPage.tsx
+++ src/components/AlertsPage.tsx
@@ -1,21 +1,18 @@
-@@ .. @@
--const AlertsPage: React.FC = () => {
-+interface AlertsPageProps {
-+  startupData?: any;
-+}
-+
-+const AlertsPage: React.FC<AlertsPageProps> = ({ startupData }) => {
-   const [alerts, setAlerts] = useState<any[]>([]);
-@@ .. @@
-   useEffect(() => {
-+    // Инициализируем данными из startup
-+    if (startupData?.alerts) {
-+      setAlerts(startupData.alerts);
-+      setLoading(false);
-+      console.log('AlertsPage initialized with startup data:', startupData.alerts.length);
-+    } else {
-+      loadAlerts();
-+    }
--    loadAlerts();
--  }, []);
-+  }, [startupData]);
+interface AlertsPageProps {
+  startupData?: any;
+}
+
+const AlertsPage: React.FC<AlertsPageProps> = ({ startupData }) => {
+  const [alerts, setAlerts] = useState<any[]>([]);
+
+  useEffect(() => {
+    // Инициализируем данными из startup
+    if (startupData?.alerts) {
+      setAlerts(startupData.alerts);
+      setLoading(false);
+      console.log('AlertsPage initialized with startup data:', startupData.alerts.length);
+    } else {
+      loadAlerts();
+    }
+  }, [startupData]);
+}
